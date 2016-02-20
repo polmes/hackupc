eb377c51c9446de4fe1d09fbeb768ea00dcebfc7
polmes <pol.developer1@gmail.com>
2016-02-20 04:41:31 +0100

    added tweets merge function

--- chrome-extension/helper.js
+++ chrome-extension/helper.js
@@ -16,3 +16,12 @@ function getTotalTweets(tweet){
 function removeIndex(tweet){
 	return tweet.text.substring(0,tweet.text.indexOf(matchIndex(tweet)))+tweet.text.substring(tweet.text.indexOf(matchIndex(tweet))+matchIndex(tweet).length,20)
 }
+
+function mergeGroupTweets(group) {
+	var finalTweet = group.tweets[group.tweets.length - 1];
+	for (var i = group.tweets.length - 2; i >= 0; i--) {
+		finalTweet.push(group.tweets[i]);
+		group.tweets[i].css('display: none');
+	}
+	return finalTweet;
+}

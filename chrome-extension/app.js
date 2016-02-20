d0fe89f33cc18a9d7d5dceef98191b05eecf30b2
polmes <pol.developer1@gmail.com>
2016-02-20 09:16:36 +0100

    something i made last night

--- chrome-extension/app.js
+++ chrome-extension/app.js
@@ -15,30 +15,37 @@
 }*/
 
 var Tweet = function(tweet) {
-	this.text = tweet.find('.tweet-text').text();
+	// this.text = tweet.find('.tweet-text').text();
+	this.html = tweet.find('.tweet-text');
+	this.text = this.html.text();
 	this.user = tweet.children().data('screen-name');
 	this.dom = tweet;
 	this.tweetNumber = getTweetNumber(tweet);
 };
 
-var tweets;
+var tweets, groups;
 
 function makeTweets() {
-	tweets.each(function(i) {
+	jQuery('[data-item-type="tweet"]').each(function(i) {
 		tweets[i] = new Tweet(jQuery('[data-item-type="tweet"]').eq(i));
+		if (typeof matchIndex(tweets[i]) === "string") {
+			groups.push(new Group(i, tweets[i].tweetNumber));
+		}
 	});
 }
 
 var Group = function(start, totalTweets) {
 	this.realUser = tweets[start].user;
 	var count = 0;
-	for (var i = start + 1; count < totalTweets; i++) {
+	for (var i = start; count < totalTweets; i++) { // begin at start to include first
 		if (tweets[i].user === realUser) {
 			this.tweets.push(tweets[i]);
 			count++;
 		}
 		// NEEDS ERROR HANDLING
 	}
+	this.finalTweet = mergeGroupTweets(this); // whatever
 };
 
 // var group = new Group()
+

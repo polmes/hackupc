1dfd069e12f63de88c88ce5867095c9d9c367ce1
Qjammer <baladoramiro+github@gmail.com>
2016-02-20 03:20:46 +0100

    added matching functions and added file to manifest

--- chrome-extension/match.js
+++ chrome-extension/match.js
@@ -7,3 +7,8 @@ function getTweetNumber(tweet){
 	var r=new RegExp("^\\d");
 	return r.exec(matchIndex(tweet));
 }
+
+function getTotalTweets(tweet){
+	var r=new RegExp("\\d$");
+	return r.exec(matchIndex(tweet));
+}

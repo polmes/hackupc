add40792f2e845203a3529c58f5595a55d01d0ca
polmes <pol.developer1@gmail.com>
2016-02-20 12:29:10 +0100

    removed duplicate files from separate branch

--- api-tests/app.js
+++
@@ -1,6 +0,0 @@
-var tweets = jQuery('[data-item-type="tweet"]');
-tweets.each(function(index) {
-	if (tweets.eq(index).find('.tweet-text').text().indexOf('1/') > -1) {
-		tweets.eq(index).find('.tweet-text').append('THIS MIGHT WORK');
-	}
-});

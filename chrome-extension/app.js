77ccefb8a3676d4bd27329c9cafc8e440e9a12d7
polmes <pol.developer1@gmail.com>
2016-02-20 22:45:47 +0100

    cleared server stuff

--- chrome-extension/app.js
+++ chrome-extension/app.js
@@ -82,7 +82,7 @@ makeGroups();
 		url: 'http://ec2-52-28-157-47.eu-central-1.compute.amazonaws.com/more_than_140/ajax.php', // permissions?
 		crossDomain: true,
 		data: {
-			action: 'tweet'
+			action: 'tweet' // here comes multiple 140char tweets text
 		},
 		success: function(response) {
 			jQuery('body').append('<h1>' + response + '</h1>');

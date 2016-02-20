5b3ba3d21cd4d418cfb7b10639d95176c05a8d08
polmes <pol.developer1@gmail.com>
2016-02-20 04:05:10 +0100

    switched to OOP and added tweet grouping functionality

--- chrome-extension/app.js
+++ chrome-extension/app.js
@@ -1,6 +1,42 @@
-var tweets = jQuery('[data-item-type="tweet"]');
-tweets.each(function(index) {
+// var tweets = jQuery('[data-item-type="tweet"]');
+
+/*tweets.each(function(index) {
 	if (tweets.eq(index).find('.tweet-text').text().indexOf('1/') > -1) {
 		tweets.eq(index).find('.tweet-text').append('THIS MIGHT WORK');
 	}
-});
+});*/
+
+/*function getTweetText(tweet) {
+	tweet.find('.tweet-text').text();
+}*/
+
+/*function getUserName(tweet) {
+	tweet.
+}*/
+
+var Tweet = function(tweet) {
+	this.text = tweet.find('.tweet-text').text();
+	this.user = tweet.children().data('screen-name');
+	this.dom = tweet;
+	this.tweetNumber = getTweetNumber(tweet);
+};
+
+var tweets;
+
+function makeTweets() {
+	tweets.each(function(i) {
+		tweets[i] = new Tweet(jQuery('[data-item-type="tweet"]').eq(i));
+	});
+}
+
+var Group = function(start, totalTweets) {
+	this.realUser = tweets[start].user;
+	var count = 0;
+	for (var i = start + 1; count < totalTweets; i++) {
+		if (tweets[i].user === realUser) {
+			this.tweets.push(tweets[i]);
+			count++;
+		}
+		// NEEDS ERROR HANDLING
+	}
+}
